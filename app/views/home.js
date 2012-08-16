ccbe1d0930ab899ad123d95b3dbac6c12593bb77
Steven Day <forestorama@gmail.com>
2012-08-16 10:41:41 +0200

    refactor slightly to make it less confusing

--- app/views/home.js
+++ app/views/home.js
@@ -4,23 +4,22 @@
 // The home view is the top level piece of UI of our app
 window.HomeView = Backbone.View.extend({
 
-	initialize: function() {
-    	this.model.on('change', this.render, this);
+  initialize: function() {
+    this.model.on('change', this.render, this);
  	}, 
 
-
-    template: _.template($("#home").html()),
-
-    render: function() {
-        $(this.el).html(this.template({sites: this.model.toJSON()}));
-        return this;
-    },
-
-    events: {
-        "vclick #bootstrap": "bootStrapSites"
-    },
-
-    bootStrapSites: function() {
-    	window.bootStrapSites();
-    }
+  template: _.template($("#home").html()),
+  
+  render: function() {
+    $(this.el).html(this.template({sites: this.model.toJSON()}));
+    return this;
+  },
+  
+  events: {
+    "vclick a#bootstrap": "bootstrap"
+  },
+  
+  bootstrap: function() {
+  	window.bootStrapSites();
+  }
 });
